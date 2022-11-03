910775db5501b95a46f235da46439933e8e19d5e
Kevin Gilio <gilio.kevin.94@gmail.com>
2022-11-03 09:51:15 +0100

    Removed user-sided confirmation, added condition to check if 18 yo or not

--- Program.cs
+++ Program.cs
@@ -9,44 +9,31 @@ namespace Esercizio_Date
         {
 
             GetdateFromInput();
-            RegisterToCityHall();
+            //RegisterToCityHall();
 
             static void GetdateFromInput()
             {
                 Console.WriteLine("Inserire una  data in formato aaaa/mm/gg\n");
                 string input = Console.ReadLine();
-
                 DateTime result;
 
                 if (DateTime.TryParse(input, out result))
                 {
-                    Console.WriteLine($" Hai inserito la seguente data: {result}\n");
+                    if (result <= DateTime.Now.AddYears(-18))
+                    {
+                        Console.WriteLine($" Hai inserito la seguente data: {result}\n Puoi procedere con l'iscrizione al comune.\n");
+                    }else if(result >= DateTime.Now.AddYears(-18))
+                    {
+                        Console.WriteLine($" Hai inserito la seguente data: {result}\n Devi essere maggiorenne per poter poterti iscrivere al comune.\n");
+                    }
                 }
                 else
                 {
-                    Console.WriteLine($"La data inserita non è valida!\n");
+                    Console.WriteLine("La data inserita non è valida!\n");
                     GetdateFromInput();
-                    
                 }
 
             }
-            static void RegisterToCityHall()
-            {
-                Console.WriteLine("Sei maggiorenne? Premi \"y\" per sì e \"n\" per no");
-                string maggiorenneConferma = Console.ReadLine();
-                if (maggiorenneConferma == "y")
-                {
-                    Console.WriteLine("Procedi con l'inserimento dei dati anagrafici per completare la registrazione");
-                }else if (maggiorenneConferma == "n")
-                {
-                    Console.WriteLine("Devi essere maggiorenne per poterti iscrivere");
-                }else{
-                    Console.WriteLine("Errore nell'inserimento");
-                    RegisterToCityHall();
-                }
-
-            }
-
         }
     }  
 }
